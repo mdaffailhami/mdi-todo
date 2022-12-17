159e46bb2bdba013986a65461c783f7216e5d004
Daffa Ilhami <mdaffailhami@gmail.com>
2022-12-17 21:36:25 +0800

    Fix import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mdi_todo/main.dart';
+import 'package:mdi_todo/presentation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
