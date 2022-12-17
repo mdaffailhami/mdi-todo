561ee0613afe29466974f954bb69b398e2ba83c1
Daffa Ilhami <mdaffailhami@gmail.com>
2022-12-18 06:52:30 +0800

    Refactoring Home Screen

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -2,13 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mdi_todo/business_logic/cubits/tasks_cubit.dart';
 import 'package:mdi_todo/business_logic/cubits/theme_mode_cubit.dart';
-import 'package:mdi_todo/data/repositories/task_repository.dart';
-import 'package:mdi_todo/data/repositories/theme_mode_repository.dart';
 
-import 'components/app_bar.dart';
-import 'components/task_form_dialog.dart';
-import 'tabs/active_task_list_tab.dart';
-import 'tabs/completed_task_list_tab.dart';
+import 'screens/home_screen.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -18,21 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  late TabController tabController;
-  final currentTabIndex = ValueNotifier(0);
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(vsync: this, length: 2);
-
-    tabController.addListener(() {
-      if (!tabController.indexIsChanging) return;
-
-      currentTabIndex.value = tabController.index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -56,41 +36,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
               brightness: Brightness.dark,
               colorSchemeSeed: const Color(0xFF00579E),
             ),
-            home: ValueListenableBuilder(
-              valueListenable: currentTabIndex,
-              builder: (context, value, child) {
-                return Scaffold(
-                  floatingActionButton: value == 0
-                      ? FloatingActionButton(
-                          onPressed: () => showDialog(
-                            context: context,
-                            builder: (context) => const MyTaskFormDialog.add(),
-                          ),
-                          tooltip: 'Add task',
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primary,
-                          child: Icon(
-                            Icons.add,
-                            color: Theme.of(context).colorScheme.onPrimary,
-                          ),
-                        )
-                      : null,
-                  body: child,
-                );
-              },
-              child: NestedScrollView(
-                headerSliverBuilder: (context, innerBoxIsScrolled) {
-                  return [MyAppBar(tabController: tabController)];
-                },
-                body: TabBarView(
-                  controller: tabController,
-                  children: const [
-                    MyActiveTaskListTab(),
-                    MyCompletedTaskListTab(),
-                  ],
-                ),
-              ),
-            ),
+            home: const MyHomeScreen(),
           );
         },
       ),
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../components/app_bar.dart';
+import '../components/task_form_dialog.dart';
+import '../tabs/active_task_list_tab.dart';
+import '../tabs/completed_task_list_tab.dart';
+
+class MyHomeScreen extends StatefulWidget {
+  const MyHomeScreen({super.key});
+
+  @override
+  State<MyHomeScreen> createState() => _MyHomeScreenState();
+}
+
+class _MyHomeScreenState extends State<MyHomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  final currentTabIndex = ValueNotifier(0);
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(vsync: this, length: 2);
+
+    tabController.addListener(() {
+      if (!tabController.indexIsChanging) return;
+
+      currentTabIndex.value = tabController.index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: currentTabIndex,
+      builder: (context, value, child) {
+        return Scaffold(
+          floatingActionButton: value == 0
+              ? FloatingActionButton(
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (context) => const MyTaskFormDialog.add(),
+                  ),
+                  tooltip: 'Add task',
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  child: Icon(
+                    Icons.add,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                )
+              : null,
+          body: child,
+        );
+      },
+      child: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [MyAppBar(tabController: tabController)];
+        },
+        body: TabBarView(
+          controller: tabController,
+          children: const [
+            MyActiveTaskListTab(),
+            MyCompletedTaskListTab(),
+          ],
+        ),
+      ),
+    );
+  }
+}
