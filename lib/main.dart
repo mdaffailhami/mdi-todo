ad47114af0595430da79fc50655d226cd12dd51c
Daffa Ilhami <mdaffailhami@gmail.com>
2022-12-21 18:09:14 +0800

    Fix bug system navbar color not changed

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,18 @@ import 'presentation/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   //Setting SysemUIOverlay
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       systemNavigationBarColor: Colors.transparent,
       systemNavigationBarDividerColor: Colors.transparent,
     ),
+  );
+
+  // Setting SystmeUIMode
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+    overlays: [SystemUiOverlay.top],
   );
 
   runApp(const MyApp());
