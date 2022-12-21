7645168a51bf35afc2bf75303821ed6cd830beaf
Daffa Ilhami <mdaffailhami@gmail.com>
2022-12-21 17:28:59 +0800

    set system navigation bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'presentation/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Setting SysemUIOverlay
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+    ),
+  );
+
   runApp(const MyApp());
 }
