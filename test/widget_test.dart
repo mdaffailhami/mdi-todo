2b1ccec84a27a83069998cf8ca7ca66ca3a800dd
Daffa Ilhami <mdaffailhami@gmail.com>
2024-11-26 05:27:29 +0700

    Remove flutter_bloc & either_dart packages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mdi_todo/old/app.dart';
+import 'package:mdi_todo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
